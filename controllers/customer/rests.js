8fd035311236449f7258b93a3ee0045c5764067a
Yang, Hao <hao-yang@uiowa.edu>
2018-04-11 01:50:54 -0500

    try whereIn function

--- controllers/customer/rests.js
+++ controllers/customer/rests.js
@@ -10,24 +10,32 @@ const handleRests = (req, res, db) => {
 
 const handleRestsList = (req, res, db) => {
     const { restTypes } = req.body;
-    var rests = [];
+    //var rests = [];
     
     if ( !restTypes ){
         return res.status(400).json('incorrect form submission');
     }
+//    
+//    for(var i=0; i<restTypes.length; i++){
+//        db.select('*').from('restaurants')
+//            .where('type', '=', restTypes[i])
+//            .then(restaurants => {
+//                //res.json(restaurants);
+//                rests = rests.concat(restaurants)
+//            })
+//            .catch(err => res.status(400).json(err))
+//    }
+//    setTimeout(function afterTwoSeconds() {
+//        res.json(rests);
+//    }, 600)
     
-    for(var i=0; i<restTypes.length; i++){
-        db.select('*').from('restaurants')
-            .where('type', '=', restTypes[i])
+    db.select('*').from('restaurants')
+            .whereIn('type', restTypes)
             .then(restaurants => {
-                //res.json(restaurants);
-                rests = rests.concat(restaurants)
+                res.json(restaurants);
+                //rests = rests.concat(restaurants)
             })
             .catch(err => res.status(400).json(err))
-    }
-    setTimeout(function afterTwoSeconds() {
-        res.json(rests);
-    }, 600)
     
     
 }
