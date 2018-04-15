9da9458378c2005e9547c6ae0012a3cdde4389b3
Yang, Hao <hao-yang@uiowa.edu>
2018-04-15 17:21:02 -0500

    modify customer signin

--- controllers/customer/signin.js
+++ controllers/customer/signin.js
@@ -7,10 +7,10 @@ const handleSignin = (req, res, db, bcrypt) => {
         return res.status(400).json('incorrect form submission');
     }
     
-    db.select('email', 'hash').from('login')
+    db.select('*').from('login')
         .where('email', '=', req.body.email)
         .then(data => {
-            const isValid = bcrypt.compareSync(password, data[0].hash);
+            const isValid = bcrypt.compareSync(password, data[1].hash);
             if (isValid) {
                 db.select('*').from('customers')
                     .where('email', '=', email)
