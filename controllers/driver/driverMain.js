1eeeb7a674e570f720feb643f16b428866168565
Yang, Hao <hao-yang@uiowa.edu>
2018-04-23 20:53:00 -0500

    get order details

--- controllers/driver/driverMain.js
+++ controllers/driver/driverMain.js
@@ -11,6 +11,8 @@ const handleDelivery = (req, res, db) => {
         trx.select('*').from('orders')
         .join('customers', 'orders.c_id', '=', 'customers.c_id')
         .join('customer_addr', 'customers.c_id', '=', 'customer_addr.c_id')
+        .join('restaurants', 'orders.r_id', '=', 'restaurants.r_id')
+        .join('restaurant_addr', 'orders.r_id', '=', 'restaurant_addr.r_id')
         .then(addr => {
             res.json(addr);
         })
