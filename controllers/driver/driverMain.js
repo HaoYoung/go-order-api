ae3fcd9e2b78b648fd4ce30c9fcff1ce00447644
Yang, Hao <hao-yang@uiowa.edu>
2018-04-30 18:32:38 -0500

    driver get orders

--- controllers/driver/driverMain.js
+++ controllers/driver/driverMain.js
@@ -1,19 +1,29 @@
 const handleDelivery = (req, res, db) => {
     
-    db.transaction(trx => {
-        trx.select('*').from('orders')
+//    db.transaction(trx => {
+//        trx.select('*').from('orders')
+//        .join('customers', 'orders.c_id', '=', 'customers.c_id')
+//        .join('customer_addr', 'customers.c_id', '=', 'customer_addr.c_id')
+//        .join('restaurants', 'orders.r_id', '=', 'restaurants.r_id')
+//        .join('restaurant_addr', 'orders.r_id', '=', 'restaurant_addr.r_id')
+//        .where('status', '=', 'wait')
+//        .then(addr => {
+//            res.json(addr);
+//        })
+//        .then(trx.commit)
+//        .catch(trx.rollback)
+//    }).catch(err => res.status(400).json(err))
+    
+    db.select('*').from('orders')
         .join('customers', 'orders.c_id', '=', 'customers.c_id')
         .join('customer_addr', 'customers.c_id', '=', 'customer_addr.c_id')
         .join('restaurants', 'orders.r_id', '=', 'restaurants.r_id')
         .join('restaurant_addr', 'orders.r_id', '=', 'restaurant_addr.r_id')
         .where('status', '=', 'wait')
-        .then(addr => {
-            res.json(addr);
+        .then(orders => {
+            res.json(orders);
         })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    }).catch(err => res.status(400).json(err))
-    
+        .catch(err => res.status(400).json('Error getting orders'))
 }
 
 module.exports = {
